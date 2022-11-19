bb6fc5f02e141f83cf6e4ea7c1b2a24722b8e89f
Forever512h <118043926+Forever512h@users.noreply.github.com>
2022-11-19 11:22:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+let number = prompt("Введите число!!!");
+switch (number) {
+  case '1':
+    alert( 'Маловато' );
+    break;
+  case '2':
+    alert( 'Маловато' );
+    break;
+  case '3':
+    alert( 'Маловато' );
+    break;
+  case '4':
+    alert( 'В точку!' );
+    break;
+  case '5':
+    alert( 'Перебор' );
+    break;
+  default:
+    alert( "Нет таких значений" );
+}
